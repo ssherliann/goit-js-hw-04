45906e8456cc1ab94cd27ebf504a2d96ada88ef9
ssherliann <137110094+ssherliann@users.noreply.github.com>
2023-11-11 23:08:51 +0200

    start

---
+++ js/task-3.js
@@ -0,0 +1,25 @@
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+
+    changeUsername(newName) {
+        this.username = newName;
+    },
+
+    updatePlayTime(hours) {
+        this.playTime = this.playTime + hours;
+    },
+
+    getInfo: function() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    }
+};
+
+
+console.log(profile.getInfo());
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); 
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); 
